873a3adcc32af288a3ca65faeb4452ad24aaf44a
Akinori MUSHA <knu@idaemons.org>
2019-09-02 19:08:36 +0900

    Update rubies to 2.6.4, 2.5.6 and 2.4.7

--- test/test_base_ruby_versions.rb
+++ test/test_base_ruby_versions.rb
@@ -36,24 +36,27 @@ class TestRubyVersions < ::Minitest::Test
     "2.4.4",
     "2.4.5",
     "2.4.6",
+    "2.4.7",
     # 2.5.x versions are currently supported.
     "2.5.0",
     "2.5.1",
     "2.5.3",
     "2.5.4",
     "2.5.5",
+    "2.5.6",
     # 2.6.0 versions are currently supported.
     "2.6.0",
     "2.6.1",
     "2.6.2",
-    "2.6.3"
+    "2.6.3",
+    "2.6.4"
   ]
 
   FASTER_VERSIONS = [
     # Test only the latest patch of each supported minor version.
-    "2.4.6",
-    "2.5.5",
-    "2.6.3"
+    "2.4.7",
+    "2.5.6",
+    "2.6.4"
   ]
 
   PREBUILT_VERSIONS = ::ENV["PREBUILT_RUBY_VERSIONS"].to_s.split(",")
